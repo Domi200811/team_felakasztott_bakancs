a3097d31bac9b1013691173bdd5acfe89ad10d83
Domi200811 <79924996+Domi200811@users.noreply.github.com>
2026-02-10 21:47:20 +0100

    Added scoreboard

--- Hangman.py
+++ Hangman.py
@@ -4,36 +4,9 @@ import random
 from contry_and_capital_list import countries_and_capitals
 from ascii import HANGMANPICS
 
-def hangmanci(city):
-    for i in city:
-        if i == ' ':
-            hidden1.append(' ')
-        else:
-            hidden1.append('-')
-    print(''.join(hidden1))
-    return city, city.count(' ')
 
-def hangmancu(country):
-    for i in country:
-        if i == ' ':
-            hidden2.append(' ')
-        else:
-            hidden2.append('-')
-    print(''.join(hidden2))
-    return country, country.count(' ')
-
-def reveal():
-    
-
-kitalalas = True
-fut = True
-hibapont = 0
-hibas = []
-hidden1 = []
-hidden2 = []
 orszag=[]
 capital=[]
-
 
 for i in countries_and_capitals():
         x, y = i.strip().split("|")
@@ -43,9 +16,9 @@ for i in countries_and_capitals():
         capital.append(y)
 ran_country= random.randint(0, (len(orszag)))
 ran_capital= random.randint(0, (len(capital)))
+print(orszag[ran_capital])
 
-
-while fut==True:
+"""while fut==True:
     city = capital[ran_capital]
     country = orszag[ran_country]
     cityconstant = city
@@ -77,4 +50,4 @@ while fut==True:
         kitalalas = False
 
 print('Köszönjük a játékot')
-print(capital[ran_capital])
+print(capital[ran_capital])"""
